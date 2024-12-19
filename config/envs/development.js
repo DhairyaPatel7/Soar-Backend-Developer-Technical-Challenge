374bc7dacd90e64d7f2c46c19424d5c79acb263e
Dhairya <dhairyapatel72@gmail.com>
2024-12-19 15:59:33 +0530

    Adding start script to package.json

--- config/envs/development.js
+++ config/envs/development.js
@@ -1,3 +1,15 @@
 module.exports = {
-    
-}
+    NODE_ENV: 'local',
+    SERVICE_NAME: 'school_management',
+    MONGO_URI: 'mongodb+srv://Dhairya:DhairyaMongodbPassword@dhairya-mongodb-cluster.jin2l.mongodb.net/school-management-system?retryWrites=true&w=majority',
+    CACHE_PREFIX: 'cache_prefix',
+    CACHE_REDIS: 'redis://:fGgCydfOIeAYkvoJH2PeLaNZ0SkGn8AD@redis-10282.c301.ap-south-1-1.ec2.redns.redis-cloud.com:10282',
+    CORTEX_PREFIX: 'cortex_prefix',
+    CORTEX_REDIS: 'redis://:fGgCydfOIeAYkvoJH2PeLaNZ0SkGn8AD@redis-10282.c301.ap-south-1-1.ec2.redns.redis-cloud.com:10282',
+    CORTEX_TYPE: 'type',
+    JWT_SECRET: 'soar-technical-school-management-secret',
+    LONG_TOKEN_SECRET: 'long_token_secret',
+    SHORT_TOKEN_SECRET: 'short_token_secret',
+    NACL_SECRET: 'nacl_secret',
+    USER_PORT: 3000
+};
